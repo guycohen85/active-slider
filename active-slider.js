65dff533e523d552c17470fc3835855d3878dc2c
guy <guycohen85@gmail.com>
2020-02-15 01:30:05 +0200

    active slider fix

--- active-slider.js
+++ active-slider.js
@@ -1,31 +1,33 @@
-export default class ActiveSlider {
+class ActiveSlider {
     constructor({
-        sliderWrapper = '',           //[required] [string] - SLIDER WRAPPER ELEMENT
-        elementsActiveClass = '',     //[required] [array OR string]  -  SELECT ELEMENTS TO ADD THE "ACTIVE" CLASS
-        swipeArea = '',               //[optional] [string] - SELECT ELEMENT THAT WILL BE THE SWIPE AREA
-        clickElementActiveClass = '', //[optional] [array OR string]  -  ON CLICK(bullet,text,etc..) SET ELEMENT AS ACTIVE(not related to prev|next buttons)
-        prevNextButtons = [],         //[optional] [array]  -  INSERT PREVIOUS AND NEXT BUTTONS ['.prev', '.next']
-        autoSlider = 3500,            //[optional] [number]  -   ACTIVATE AUTOSLIDER , IF 'FALSE' THEN NO AUTOSLIDE - (number=milisecond)
-        loop = true                   //[optional] [boolean]  -  LOOP SLIDER(default true)
-    }){
+        sliderWrapper = '', //(string)(required) - Select the slider wrapper element. example: '.slider'
+        activeElements = '', //(array|string)(required) - Select elements to add the active class. example: ['.slide-image', '.bullet']
+        swipeArea = '', //(string)(optional) - Select the element that will be the swipe area. example: '.slide-images-container'
+        clickableElements = '', //(array|string)(optional) - Add the active class to the clicked slide. example: '.bullet'
+        prevNextButtons = false, //(array)(optional) - Select the previous and next buttons. example: ['.prev', '.next']
+        autoSlide = 3500, //(number|boolean)(optional) - Default time between slides is 3500 milliseconds. set to false if no need an auto slide. default: 3500
+        stopAutoSlideOnClick = false, //(array)(optional) - Select the elements to be the listeners for the click event in order to stop the auto slide. example: ['.prev', '.next']
+        loop = true //Loop slider (default true) default: true
+    }) {
         this.$slider = document.querySelector(sliderWrapper);
         this.playSlider;
         this.$prevBtn;
         this.$nextBtn;
-        this.$swipeArea = (swipeArea) ? document.querySelector(swipeArea)  : false ;
-        this.$slideClickableItems = clickElementActiveClass;
+        this.$swipeArea = (swipeArea) ? document.querySelector(swipeArea) : false;
+        this.clickableElements = clickableElements;
+        this.stopAutoSlideOnClick = stopAutoSlideOnClick;
         this.loop = loop;
         this.slideClassName = '';
 
         this.$slider.setAttribute("data-slide", 1);
-        
+
         //All elements that will be asiggned the "active" class(images,bullets,titles,etc...)
-        if( Array.isArray(elementsActiveClass) ){
-            this.slideItems = elementsActiveClass.map( slideItem => this.$slider.querySelectorAll( slideItem ));
-            this.slideClassName = elementsActiveClass[0];
-        }else{
-            this.slideItems = this.$slider.querySelectorAll( elementsActiveClass );
-            this.slideClassName = elementsActiveClass;
+        if (Array.isArray(activeElements)) {
+            this.slideItems = activeElements.map(slideItem => this.$slider.querySelectorAll(slideItem));
+            this.slideClassName = activeElements[0];
+        } else {
+            this.slideItems = this.$slider.querySelectorAll(activeElements);
+            this.slideClassName = activeElements;
         }
         this.slideCount = this.slideItems[0].length;
 
@@ -33,13 +35,13 @@ export default class ActiveSlider {
         this.setFirstSlideActiveClass();
 
         //Autoplay
-        this.autoSliderTime = autoSlider;
-        if(this.autoSliderTime){
+        this.autoSliderTime = autoSlide;
+        if (this.autoSliderTime) {
             this.autoPlaySlider();
         }
 
         //Add |prev|next| buttons
-        if(prevNextButtons){
+        if (prevNextButtons) {
             this.addPrevNextButtons(prevNextButtons);
         }
 
@@ -47,7 +49,7 @@ export default class ActiveSlider {
         this.buttonsClick();
     }
 
-    addPrevNextButtons(prevNextButtons){
+    addPrevNextButtons(prevNextButtons) {
         this.$prevBtn = document.querySelector(prevNextButtons[0]);
         this.$prevBtn.addEventListener('click', () => {
             this.prevSlide();
@@ -58,38 +60,46 @@ export default class ActiveSlider {
         });
     }
 
-    autoPlaySlider(){
+    autoPlaySlider() {
         var self = this;
+        
         //Start autoslide
-        this.playSlider = setInterval( () => {
+        this.playSlider = setInterval(() => {
             this.nextSlide();
         }, this.autoSliderTime);
-        //Stop autoslide
-        this.$slider.addEventListener("click", removeAutoSlide);
-        this.$slider.addEventListener("touchstart", removeAutoSlide);
 
-        function removeAutoSlide(e){
+        //Stop autoslide
+        if(this.stopAutoSlideOnClick){
+            for (let io = 0; io < this.stopAutoSlideOnClick.length; io++) {
+                var elements = document.querySelectorAll(this.stopAutoSlideOnClick[io]);
+                for (let i = 0; i < elements.length; i++) {
+                    elements[i].addEventListener("click", removeAutoSlide);
+                    elements[i].addEventListener("touchstart", removeAutoSlide);
+                }
+            }
+        }
+
+        function removeAutoSlide(e) {
             self.$slider.removeEventListener(e.type, removeAutoSlide);
             clearInterval(self.playSlider);
         }
-
     }
 
-    buttonsClick(){
+    buttonsClick() {
         var self = this;
 
         // On click(bullet,text,etc..) set element as ACTIVE
-        if(this.$slideClickableItems){
-            if( Array.isArray(this.$slideClickableItems) ){
-                this.$slideClickableItems.forEach(function(items){
-                    document.querySelectorAll(items).forEach( el => {
+        if (this.clickableElements) {
+            if (Array.isArray(this.clickableElements)) {
+                this.clickableElements.forEach(function (items) {
+                    document.querySelectorAll(items).forEach(el => {
                         el.addEventListener('click', (e) => {
                             self.setSlideActiveClassByClick(e);
                         });
                     });
                 });
-            }else{
-                document.querySelectorAll(this.$slideClickableItems).forEach( el => {
+            } else {
+                document.querySelectorAll(this.clickableElements).forEach(el => {
                     el.addEventListener('click', (e) => {
                         self.setSlideActiveClassByClick(e);
                     });
@@ -97,55 +107,55 @@ export default class ActiveSlider {
             }
         }
         //touch events
-        if(this.$swipeArea){
+        if (this.$swipeArea) {
             this.touchstartX = 0;
-            this.$swipeArea.addEventListener('touchstart', function(event) {
+            this.$swipeArea.addEventListener('touchstart', function (event) {
                 this.touchstartX = event.changedTouches[0].screenX;
             }, false);
 
-            this.$swipeArea.addEventListener('touchend', function(event) {
+            this.$swipeArea.addEventListener('touchend', function (event) {
                 var touchendX = event.changedTouches[0].screenX;
-                if (touchendX <= this.touchstartX && (this.touchstartX - touchendX) > 25 ) {
-                    self.nextSlide();//Swiped left
+                if (touchendX <= this.touchstartX && (this.touchstartX - touchendX) > 25) {
+                    self.nextSlide(); //Swiped left
                 }
-                
+
                 if (touchendX >= this.touchstartX && (touchendX - this.touchstartX) > 25) {
-                    self.prevSlide();//Swiped right
+                    self.prevSlide(); //Swiped right
                 }
             }, false);
         }
     }
 
-    removeAllActiveClass(){
-        this.slideItems.forEach( slideItem => {
+    removeAllActiveClass() {
+        this.slideItems.forEach(slideItem => {
             slideItem.forEach(el => {
                 el.classList.remove("active");
             });
         });
     }
 
-    setFirstSlideActiveClass(){
+    setFirstSlideActiveClass() {
         this.removeAllActiveClass();
         this.$slider.dataset.slide = 1;
-        this.slideItems.forEach( slideItem => {
+        this.slideItems.forEach(slideItem => {
             slideItem[0].classList.add("active");
         });
     }
 
-    setLastSlideActiveClass(){
+    setLastSlideActiveClass() {
         this.removeAllActiveClass();
         this.$slider.dataset.slide = this.slideCount;
-        this.slideItems.forEach( slideItem => {
+        this.slideItems.forEach(slideItem => {
             slideItem[slideItem.length - 1].classList.add("active");
         });
     }
 
-    setNextSlideActiveClass(){
+    setNextSlideActiveClass() {
         this.$slider.dataset.slide = parseInt(this.$slider.dataset.slide) + 1;
-        this.slideItems.forEach( slideItem => {
+        this.slideItems.forEach(slideItem => {
 
             for (let i = 0; i < this.slideCount; i++) {
-                if(slideItem[i].classList.contains('active')){
+                if (slideItem[i].classList.contains('active')) {
                     slideItem[i].classList.remove('active');
                     slideItem[i].nextElementSibling.classList.add('active');
                     break;
@@ -154,12 +164,12 @@ export default class ActiveSlider {
         });
     }
 
-    setPrevSlideActiveClass(){
-        this.$slider.dataset.slide = ( parseInt(this.$slider.dataset.slide) === 1 )? this.slideCount : parseInt(this.$slider.dataset.slide) - 1;
-        this.slideItems.forEach( slideItem => {
+    setPrevSlideActiveClass() {
+        this.$slider.dataset.slide = (parseInt(this.$slider.dataset.slide) === 1) ? this.slideCount : parseInt(this.$slider.dataset.slide) - 1;
+        this.slideItems.forEach(slideItem => {
 
             for (let i = 0; i < this.slideCount; i++) {
-                if(slideItem[i].classList.contains('active')){
+                if (slideItem[i].classList.contains('active')) {
                     slideItem[i].classList.remove('active');
                     slideItem[i].previousElementSibling.classList.add('active');
                     break;
@@ -168,49 +178,48 @@ export default class ActiveSlider {
         });
     }
 
-    setSlideActiveClassByClick(event){
+    setSlideActiveClassByClick(event) {
         var slideIndex = '';
         this.removeAllActiveClass();
 
-        for(let io = 0; io < this.slideCount; io++){
+        for (let io = 0; io < this.slideCount; io++) {
             for (let i = 0; i < this.slideCount; i++) {
-                if(event.target.isSameNode( this.slideItems[io][i] )){
+                if (event.target.isSameNode(this.slideItems[io][i])) {
                     slideIndex = i;
                     this.$slider.dataset.slide = slideIndex + 1;
                     break;
                 }
             }
-            if(slideIndex){
+            if (slideIndex) {
                 break;
             }
         }
 
-        for(let io = 0; io < this.slideCount; io++){
+        for (let io = 0; io < this.slideCount; io++) {
             this.slideItems[io][slideIndex].classList.add('active');
         }
     }
 
-    nextSlide(){
+    nextSlide() {
         var slideLastOfType = this.$slider.querySelector(this.slideClassName + '.active' + ':last-of-type');
-        if(slideLastOfType && !this.loop){
+        if (slideLastOfType && !this.loop) {
             //do nothing
-        }else if(slideLastOfType){
+        } else if (slideLastOfType) {
             this.setFirstSlideActiveClass();
-        }else{
+        } else {
             this.setNextSlideActiveClass();
         }
     }
 
-    prevSlide(){
+    prevSlide() {
         var slideFirstOfType = this.$slider.querySelector(this.slideClassName + '.active' + ':first-of-type');
-        if(slideFirstOfType && !this.loop){
+        if (slideFirstOfType && !this.loop) {
             //do nothing
-        }else if(slideFirstOfType){
+        } else if (slideFirstOfType) {
             this.setLastSlideActiveClass();
-        }else{
+        } else {
             this.setPrevSlideActiveClass();
         }
     }
 
 }
-
